75a37f39331e388e8c38526bca0d66d66a8e6bf8
Munim Akbar <98738274+MunimAkbar@users.noreply.github.com>
2025-06-06 00:24:41 +0500

    Update predict_video.py

--- scripts/predict_video.py
+++ scripts/predict_video.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('C:/Users/cowlar/Downloads/Vehicle_Detection_using_YOLOv8/TrainingResults/vehiclesDetection2/weights/best.pt')
+model = YOLO('C:/Users/Downloads/Vehicle_Detection_using_YOLOv8/TrainingResults/vehiclesDetection2/weights/best.pt')
 
 model.predict(
-    source='C:/Users/cowlar/Downloads/Vehicle_Detection_using_YOLOv8/TestVideo',
+    source='C:/Users/Downloads/Vehicle_Detection_using_YOLOv8/TestVideo',
     conf=0.75,
     save=True
 )
