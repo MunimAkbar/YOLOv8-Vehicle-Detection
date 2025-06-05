ce5520cd213d40ca09edb6fc0e0e80c92272d9fa
Munim Akbar <98738274+MunimAkbar@users.noreply.github.com>
2025-06-06 00:23:52 +0500

    Update predict_images.py

--- scripts/predict_images.py
+++ scripts/predict_images.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('C:/Users/cowlar/Downloads/Vehicle_Detection_using_YOLOv8/TrainingResults/vehiclesDetection2/weights/best.pt')
+model = YOLO('C:/Users/Downloads/Vehicle_Detection_using_YOLOv8/TrainingResults/vehiclesDetection2/weights/best.pt')
 
 model.predict(
-    source = 'C:/Users/cowlar/Downloads/Vehicle_Detection_using_YOLOv8/VehiclesDetectionDataset/test/images',
+    source = 'C:/Users/Downloads/Vehicle_Detection_using_YOLOv8/VehiclesDetectionDataset/test/images',
     conf=0.5,
     save=True
 )
